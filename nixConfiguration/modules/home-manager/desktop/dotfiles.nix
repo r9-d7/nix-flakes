f1432dcee0a25ed80b82bf15d44f5ec7f0b098ff
r9-d7 <ross.fagerli@gmail.com>
2024-12-05 11:35:59 -0500

    fixed paths to dotfiles

--- nixConfiguration/modules/home-manager/desktop/dotfiles.nix
+++ nixConfiguration/modules/home-manager/desktop/dotfiles.nix
@@ -1,43 +1,43 @@
 {
   # hyprland config
-  xdg.configFile."hypr/hyprland.conf".source = ../dotfiles/hyprland/.config/hypr/hyprland.conf;
-  xdg.configFile."hypr/mocha.conf".source = ../dotfiles/hyprland/.config/hypr/mocha.conf;
-  xdg.configFile."hypr/monitors.conf".source = ../dotfiles/hyprland/desktop/.config/hypr/monitors.conf;
-  xdg.configFile."hypr/workspaces.conf".source = ../dotfiles/hyprland/.config/hypr/workspaces.conf;
-  xdg.configFile."hypr/gamemode.sh".source = ../dotfiles/hyprland/.config/hypr/gamemode.sh;
+  xdg.configFile."hypr/hyprland.conf".source = ../../dotfiles/hyprland/.config/hypr/hyprland.conf;
+  xdg.configFile."hypr/mocha.conf".source = ../../dotfiles/hyprland/.config/hypr/mocha.conf;
+  xdg.configFile."hypr/monitors.conf".source = ../../dotfiles/hyprland/desktop/.config/hypr/monitors.conf;
+  xdg.configFile."hypr/workspaces.conf".source = ../../dotfiles/hyprland/.config/hypr/workspaces.conf;
+  xdg.configFile."hypr/gamemode.sh".source = ../../dotfiles/hyprland/.config/hypr/gamemode.sh;
 
 
   # hypridle
-  xdg.configFile."hypr/hypridle.conf".source = ../dotfiles/hypridle/.config/hypr/hypridle.conf;
+  xdg.configFile."hypr/hypridle.conf".source = ../../dotfiles/hypridle/.config/hypr/hypridle.conf;
 
   # hyprlock
-  xdg.configFile."hypr/hyprlock.conf".source = ../dotfiles/hyprlock/.config/hypr/hyprlock.conf;
-  xdg.configFile."hypr/profilePics/photos/Shuttle.png".source = ../dotfiles/hyprlock/.config/hypr/profilePics/photos/Shuttle.png;
-  xdg.configFile."hypr/wallpapers/galaxy.jpeg".source = ../dotfiles/hyprpaper/.config/hypr/wallpapers/galaxy.jpeg;
+  xdg.configFile."hypr/hyprlock.conf".source = ../../dotfiles/hyprlock/.config/hypr/hyprlock.conf;
+  xdg.configFile."hypr/profilePics/photos/Shuttle.png".source = ../../dotfiles/hyprlock/.config/hypr/profilePics/photos/Shuttle.png;
+  xdg.configFile."hypr/wallpapers/galaxy.jpeg".source = ../../dotfiles/hyprpaper/.config/hypr/wallpapers/galaxy.jpeg;
 
   # hyprpaper
-  xdg.configFile."hypr/wallpapers/winterforest.jpeg".source = ../dotfiles/hyprpaper/.config/hypr/wallpapers/winterforest.jpeg;
-  xdg.configFile."hypr/hyprpaper.conf".source = ../dotfiles/hyprpaper/.config/hypr/hyprpaper.conf;
+  xdg.configFile."hypr/wallpapers/winterforest.jpeg".source = ../../dotfiles/hyprpaper/.config/hypr/wallpapers/winterforest.jpeg;
+  xdg.configFile."hypr/hyprpaper.conf".source = ../../dotfiles/hyprpaper/.config/hypr/hyprpaper.conf;
 
   #kitty
-  xdg.configFile."kitty/kitty.conf".source = ../dotfiles/kitty/.config/kitty/kitty.conf;
-  xdg.configFile."kitty/current-theme.conf".source = ../dotfiles/kitty/.config/kitty/current-theme.conf;
+  xdg.configFile."kitty/kitty.conf".source = ../../dotfiles/kitty/.config/kitty/kitty.conf;
+  xdg.configFile."kitty/current-theme.conf".source = ../../dotfiles/kitty/.config/kitty/current-theme.conf;
 
   #waybar
-  xdg.configFile."waybar/config.jsonc".source = ../dotfiles/waybar/desktop/.config/waybar/config.jsonc;
-  xdg.configFile."waybar/mocha.css".source = ../dotfiles/waybar/.config/waybar/mocha.css;
-  xdg.configFile."waybar/style.css".source = ../dotfiles/waybar/desktop/.config/waybar/style.css;
+  xdg.configFile."waybar/config.jsonc".source = ../../dotfiles/waybar/desktop/.config/waybar/config.jsonc;
+  xdg.configFile."waybar/mocha.css".source = ../../dotfiles/waybar/.config/waybar/mocha.css;
+  xdg.configFile."waybar/style.css".source = ../../dotfiles/waybar/desktop/.config/waybar/style.css;
 
   # starship
-  xdg.configFile."starship.toml".source = ../dotfiles/starship/.config/starship.toml;
+  xdg.configFile."starship.toml".source = ../../dotfiles/starship/.config/starship.toml;
   
   # wofi
-  xdg.configFile."wofi/style.css".source = ../dotfiles/wofi/.config/wofi/style.css;
+  xdg.configFile."wofi/style.css".source = ../../dotfiles/wofi/.config/wofi/style.css;
 
   # user scripts
-  xdg.dataFile.".userscripts/brightct.sh".source = ../dotfiles/scripts/.userscripts/brightct.sh;
-  xdg.dataFile.".userscripts/chbright.sh".source = ../dotfiles/scripts/.userscripts/chbright.sh;
-  xdg.dataFile.".userscripts/mountonedrive.sh".source = ../dotfiles/scripts/.userscripts/mountonedrive.sh;
+  xdg.dataFile.".userscripts/brightct.sh".source = ../../dotfiles/scripts/.userscripts/brightct.sh;
+  xdg.dataFile.".userscripts/chbright.sh".source = ../../dotfiles/scripts/.userscripts/chbright.sh;
+  xdg.dataFile.".userscripts/mountonedrive.sh".source = ../../dotfiles/scripts/.userscripts/mountonedrive.sh;
 
 
 
