bd23c1b3fbdbea87cfcebeae805e51ecbf9275c6
r9-d7 <ross.fagerli@gmail.com>
2024-12-06 14:57:46 -0500

    removed deprecated config option

--- nixOSconfig/modules/nixosModules/nvidia.nix
+++ nixOSconfig/modules/nixosModules/nvidia.nix
@@ -1,11 +1,6 @@
 { config, pkgs, lib, ... }: 
 {
-  hardware.graphics = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
+  hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
