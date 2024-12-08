c5f8ea5eb00a883d91074d07f4a49c5e9d3cf7f4
r9-d7 <ross.fagerli@gmail.com>
2024-12-08 17:21:26 -0500

    changed starship colors

--- nixOSconfig/modules/nixosModules/nvidia.nix
+++ nixOSconfig/modules/nixosModules/nvidia.nix
@@ -3,6 +3,7 @@
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
-  hardware.nvidia.nvidiaSettings = true; 
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  hardware.nvidia.nvidiaSettings = true;
+  hardware.nvidia.open = false;
 }
