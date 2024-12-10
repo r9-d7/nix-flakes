1ee2144800967458572544cc9453b7026d3ee6a6
r9-d7 <ross.fagerli@gmail.com>
2024-12-10 10:36:00 -0500

    new vscode theme

--- nixOSconfig/modules/homeManagerModules/code/vscode.nix
+++ nixOSconfig/modules/homeManagerModules/code/vscode.nix
@@ -5,6 +5,7 @@
     enable = true;
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
+      enkia.tokyo-night
       vscodevim.vim
       yzhang.markdown-all-in-one
     ];
